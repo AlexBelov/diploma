08efc6be825e9dfe90c4a27b78eb2e32b16af82a
Alexander Belov <git@belov.by>
2017-12-20 13:06:50 -0800

    Add spaces to train_model.py

--- lib/train_model.py
+++ lib/train_model.py
@@ -7,9 +7,11 @@ from sklearn.externals import joblib
 data = pd.read_csv("~/github_stars_november.csv", sep=',', header=1, names=["user", "repo"])
 data['user'] = data['user'].astype("category")
 data['repo'] = data['repo'].astype("category")
+
 stars = coo_matrix((np.ones(data.shape[0]),
                    (data['repo'].cat.codes.copy(),
                     data['user'].cat.codes.copy())))
+
 model = AlternatingLeastSquares(factors=50,
                                 regularization=0.01,
                                 dtype=np.float64,
